5115afd5f412a8ed1bacd4c1a167c7256b7e6942
Rohan Barman <rohanbarman100@gmail.com>
2023-12-21 00:15:20 +0530

    Added proxy to the package.json to resolve cors error in production

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -2,8 +2,7 @@ export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=50&chart=mostPopular&regionCode=IN&key=" +
   process.env.REACT_APP_KEY;
 
-export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "/complete/search?client=firefox&ds=yt&q=";
 
 export const YOUTUBE_SEARCH_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=" +
